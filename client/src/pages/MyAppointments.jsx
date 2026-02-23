04b082941d35469f40814a4912bc0a4de5fa1a9a
Jay Kishan Saw <116188041+Jaysaw@users.noreply.github.com>
2026-02-23 15:50:01 +0530

    Update MyAppointments.jsx

--- client/src/pages/MyAppointments.jsx
+++ client/src/pages/MyAppointments.jsx
@@ -22,7 +22,7 @@ export default function MyAppointments() {
     useEffect(() => {
         const fetchAppointments = async () => {
             const res = await axios.get(
-                "http://https://meditrack-t4fq.onrender.com/api/appointments",
+                "https://meditrack-su56.onrender.com/api/appointments",
                 { headers: { Authorization: `Bearer ${token}` } }
             );
             setAppointments(res.data);
